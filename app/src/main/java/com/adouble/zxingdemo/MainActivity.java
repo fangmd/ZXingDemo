4c624744c6ed39acee9c72e97d9816671d6b1d39
fangmd <fangmingdong0224@gmail.com>
2016-05-02 18:02:33 +0800

    --

--- app/src/main/java/com/adouble/zxingdemo/MainActivity.java
+++ app/src/main/java/com/adouble/zxingdemo/MainActivity.java
@@ -2,8 +2,9 @@ package com.adouble.zxingdemo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -36,12 +37,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.qrcode_dencode: //扫描
                 Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivityForResult(intent, REQUEST_CODE);
                 break;
             case R.id.qrcode_encode: //生成
                 try {
-                    Bitmap mBitmap = EncodingHandler.createQRCode("www.baidu.com", 300);
-                    qrcodeImg.setImageBitmap(mBitmap);
+//                    Bitmap mBitmap = EncodingHandler.createQRCode("www.baidu.com", 300);
+//                    qrcodeImg.setImageBitmap(mBitmap);
+                    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.head);
+                    Bitmap www = EncodingHandler.createQRCode("www", 600, 600, bitmap);
+                    qrcodeImg.setImageBitmap(www);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
